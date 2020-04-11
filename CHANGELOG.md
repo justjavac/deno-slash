4cd85d60060516b8a2d98af1ab335df563790bcd
justjavac <justjavac@gmail.com>
2020-04-11 10:05:21 +0800

    bump deno@0.40.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.0 - [2020-04-11]
+
+- bump deno 0.40.0
+
 ## 0.1.0 - [2019-11-30]
 
 first release
--- slash_test.ts
+++ slash_test.ts
@@ -1,28 +1,27 @@
-import { test } from "https://deno.land/std@v0.24.0/testing/mod.ts";
-import { assertEquals } from "https://deno.land/std@v0.24.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
 import slash from "./mod.ts";
 
-test({
+Deno.test({
   name: "convert backwards-slash paths to forward slash paths",
   fn(): void {
     assertEquals(slash("c:/aaaa\\bbbb"), "c:/aaaa/bbbb");
     assertEquals(slash("c:\\aaaa\\bbbb"), "c:/aaaa/bbbb");
-  }
+  },
 });
 
-test({
+Deno.test({
   name: "not convert extended-length paths",
   fn(): void {
     const path = "\\\\?\\c:\\aaaa\\bbbb";
     assertEquals(slash(path), path);
-  }
+  },
 });
 
-test({
+Deno.test({
   name: "not convert paths with Unicode",
   fn(): void {
     const path = "c:\\aaaa\\bbbb\\★";
     assertEquals(slash(path), path);
-  }
+  },
 });
