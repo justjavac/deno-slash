d837efd994a428f397d75d12a574b30fcfd9afb5
迷渡 <justjavac@gmail.com>
2020-09-05 23:59:59 +0800

    upgrade std@0.68.0

--- slash_test.ts
+++ slash_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.68.0/testing/asserts.ts";
 
 import slash from "./mod.ts";
 
